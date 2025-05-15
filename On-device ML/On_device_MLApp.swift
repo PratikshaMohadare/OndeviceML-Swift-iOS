efbfc85e44da7c1ae3a8160be3bb47e44e748c1b
Pratiksha <pratiksha.mohadare9@gmail.com>
2025-05-15 17:47:47 +0530

    Initial Commit

---
+++ On-device ML/On_device_MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_device_MLApp.swift
+//  On-device ML
+//
+//  Created by Pratiksha on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct On_device_MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
